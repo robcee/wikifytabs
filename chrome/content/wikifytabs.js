c9728ffaeaeb85bf6ee190a7a8aad1510115e7c2
Rob Campbell <rcampbell@mozilla.com>
2010-12-15 19:21:42 -0800

    first crack at 1.0

--- chrome/content/wikifytabs.js
+++ chrome/content/wikifytabs.js
@@ -1,48 +1,62 @@
-var gWikifyTabs;
+/*
+ * WikifyTabs
+ * Provide convenient, copyable lists of open tabs per browser window.
+ * Author: Rob Campbell <rob@antennasoft.net>
+ * Versions:
+ *   1.0 - Added submenus and bbcode, HTML options.
+ *
+ * All Copyright Dedicated to the Public Domain 2010.
+ */
+
+var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+
+const WIKI = 1;
+const BBCODE = 2;
+const HTML = 3;
+
+var html =
+  "<h1 style='font: 14em/16em sans-serif'>Here are your links</h1>\n" +
+  "<pre style='border: 2px solid #333; border-radius: 4px;'>$2</pre>";
 
 function url(spec) {
-  var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
   return ios.newURI(spec, null, null);
 }
 
-function _getTabUris() {
-    var tabList = [];
+function getTabUris() {
+  let tabList = [];
 
-    var browsers = gBrowser.browsers;
-    for (var i = 0; i < browsers.length; ++i) {
-        try {
-            var bUri = browsers[i].webNavigation.currentURI.spec;
-            var bTitle = browsers[i].contentDocument.title
-            var uriTitlePair = [bUri, bTitle];
-            tabList.push(uriTitlePair);
-        } catch (e) { }
-    }
-    return tabList;
+  let browsers = gBrowser.browsers;
+  for (let i = 0; i < browsers.length; ++i) {
+    try {
+      let bUri = browsers[i].webNavigation.currentURI.spec;
+      let bTitle = browsers[i].contentDocument.title
+      let uriTitlePair = [bUri, bTitle];
+      tabList.push(uriTitlePair);
+    } catch (e) { }
+  }
+  return tabList;
 }
 
-function onTabOpen(tabs) {
-    var tabList = _getTabUris();
-    var textContent = "<h1>copy the links below to your wiki</h1>\n<pre>";
-    for (var i = 0; i < tabList.length; i++) {
-        textContent += "* [" + tabList[i][0] + " " + tabList[i][1] + "]\n";
-    }
-    textContent += "</pre>";
-    gBrowser.contentDocument.title = "WikifiedTabs";
-    gBrowser.contentDocument.body.innerHTML = textContent;
+function onTabOpen() {
+  gBrowser.selectedBrowser.removeEventListener("load", onTabOpen, false);
+  let tabList = getTabUris();
+  let textContent = new String(html);
+  let tabText = new String();
+
+  for (let i = 0; i < tabList.length; i++)
+    tabText += "* [" + tabList[i][0] + " " + tabList[i][1] + "]\n";
+
+  gBrowser.contentDocument.title = "WikifiedTabs";
+
+  textContent.replace("$2", tabText);
+
+  gBrowser.contentDocument.body.innerHTML = textContent;
 }
 
 function writeTabsInText() {
-    newTab = gBrowser.addTab();
-    gBrowser.selectedTab = newTab;
-    gBrowser.focus();
-    var newBrowser = gBrowser.getBrowserForTab(newTab);
-    newBrowser.addEventListener("load", onTabOpen, true);
+  let newTab = gBrowser.addTab();
+  gBrowser.selectedTab = newTab;
+  // gBrowser.focus();
+  let newBrowser = gBrowser.getBrowserForTab(newTab);
+  newBrowser.addEventListener("load", onTabOpen, false);
 }
-
-function onWikifyTabsLoad() {
-    // do initialization here
-    gWikifyTabs = "loaded";
-}
-
-//Make sure we load on start-up of browser.
-window.addEventListener('load', onWikifyTabsLoad, false);
