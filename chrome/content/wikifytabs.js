a624bd57997ff14d61215c17261f4600e9a263bb
Rob Campbell <rcampbell@mozilla.com>
2010-12-15 22:43:54 -0800

    wikifying operational. saving work

--- chrome/content/wikifytabs.js
+++ chrome/content/wikifytabs.js
@@ -15,18 +15,19 @@ const BBCODE = 2;
 const HTML = 3;
 
 var html =
-  "<h1 style='font: 14em/16em sans-serif'>Here are your links</h1>\n" +
-  "<pre style='border: 2px solid #333; border-radius: 4px;'>$2</pre>";
+  "<h1 style='font: 24px/26px sans-serif'>Links for Tabs</h1>\n" +
+  "<pre style='border: 2px solid #333; border-radius: 4px; margin: 12px; padding: 8px;'>$2</pre>";
 
 function url(spec) {
   return ios.newURI(spec, null, null);
 }
 
 function getTabUris() {
+  _log("getTabUris");
   let tabList = [];
 
   let browsers = gBrowser.browsers;
-  for (let i = 0; i < browsers.length; ++i) {
+  for (let i = 0; i < browsers.length - 1; ++i) {
     try {
       let bUri = browsers[i].webNavigation.currentURI.spec;
       let bTitle = browsers[i].contentDocument.title
@@ -38,7 +39,7 @@ function getTabUris() {
 }
 
 function onTabOpen() {
-  // gBrowser.selectedBrowser.removeEventListener("load", onTabOpen, false);
+  _log("onTabOpen");
   let tabList = getTabUris();
   let textContent = new String(html);
   let tabText = new String();
@@ -46,17 +47,26 @@ function onTabOpen() {
   for (let i = 0; i < tabList.length; i++)
     tabText += "* [" + tabList[i][0] + " " + tabList[i][1] + "]\n";
 
+  _log("tabText: " + tabText);
+
   gBrowser.contentDocument.title = "WikifiedTabs";
 
-  textContent.replace("$2", tabText);
+  textContent = textContent.replace("$2", tabText);
+  _log("textContent: " + textContent);
 
   gBrowser.contentDocument.body.innerHTML = textContent;
 }
 
 function writeTabsInText() {
-  let newTab = gBrowser.addTab();
-  gBrowser.selectedTab = newTab;
-  // gBrowser.focus();
-  let newBrowser = gBrowser.getBrowserForTab(newTab);
-  onTabOpen();
+  _log("writeTabsInText");
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function() {
+    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
+    _log("load listener");
+    setTimeout(onTabOpen, 5);
+  }, true);
+}
+
+function _log(msg) {
+  Services.console.logStringMessage("LOG: " + msg);
 }
